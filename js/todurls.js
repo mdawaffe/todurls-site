b2036d8339a71d650dcdd5da34e2e50876bb0c60
Michael D Adams <mda@blogwaffe.com>
2013-02-20 17:59:37 -0800

    token is stored in cookie managed by Extension.  Add Get Started and Help notices.

--- js/todurls.js
+++ js/todurls.js
@@ -1,7 +1,7 @@
 window.TodURLsClient = (function() {
 var client = {
 	appID: 'drugs-escapes-418',
-	token: '0b4e4c04eb774cd28ef953c593eda10c',
+	token: null,
 	simperium: null,
 	bucket: null,
 
@@ -11,8 +11,21 @@ var client = {
 	app: null,
 
 	initialize: function() {
+		this.getCookie();
+
+		if ( ! this.token ) {
+			jQuery( '#get-started' ).show();
+
+			setTimeout( function() {
+				TodURLsClient.initialize.call( TodURLsClient );
+			}, 5000 );
+			return;
+		}
+
+		jQuery( '#get-started' ).hide();
+
 		this.simperium = new Simperium( client.appID, {
-			token: client.token,
+			token: client.token
 		} );
 
 		this.bucket = this.simperium.bucket( 'todurls' );
@@ -22,7 +35,7 @@ var client = {
 				url        : '',
 				image      : '',
 				title      : '',
-				description: '',
+				description: ''
 			},
 
 			initialize: function() {
@@ -34,6 +47,16 @@ var client = {
 
 		var TodoList = Backbone.SimperiumCollection.extend( {
 			model: Todo,
+
+			remove: function() {
+				Backbone.SimperiumCollection.prototype.remove.apply( this, arguments );
+
+				if ( ! this.isEmpty() ) {
+					return;
+				}
+
+				this.trigger( 'empty' );
+			},
 		} );
 
 		this.collection = new TodoList( [], { bucket: client.bucket } );
@@ -44,7 +67,7 @@ var client = {
 			template: _.template( jQuery( '#template-todurl' ).html() ),
 
 			events: {
-				'click span' : 'delete',
+				'click span' : 'delete'
 			},
 
 			initialize: function() {
@@ -59,31 +82,48 @@ var client = {
 
 			delete: function() {
 				this.model.trigger( 'delete' );
-			},
+			}
 		} );
 
 		var AppView = Backbone.View.extend( {
-			el: jQuery( '#todurls' ),
+			el    : jQuery( '#todurls' ),
+			helpEl: jQuery( '#help' ),
 			listEl: null,
 
 			initialize: function() {
 				this.listEl = this.$( '#todurls-list' );
+				this.helpEl.show();
 
 				this.listenTo( client.collection, 'add',   this.addOne );
 				this.listenTo( client.collection, 'reset', this.addAll );
+				this.listenTo( client.collection, 'empty', this.displayHelp );
 			},
 
 			addOne: function( todo ) {
+				this.helpEl.hide();
 				var view = new TodoView( { model: todo } );
 				this.listEl.prepend( view.render().el );
 			},
 
 			addAll: function() {
 				client.collection.each( this.addOne, this );
+			},
+
+			displayHelp: function() {
+				this.helpEl.show();
 			}
 		} );
 
 		this.app = new AppView;
+	},
+
+	getCookie: function() {
+		var token = document.cookie.match( /token=[^;]*(?=;|$)/ );
+		if ( ! token ) {
+			return;
+		}
+
+		this.token = token[0].split( '=' )[1];
 	}
 };
 
